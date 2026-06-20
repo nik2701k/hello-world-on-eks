3497421c75a18e1845c7cfd6d2e696c3ba38c912
nikunj <nikunjb@locus.sh>
2026-06-20 18:37:55 +0530

    Add /healthz endpoint for liveness probe

--- app/main.go
+++ app/main.go
@@ -9,5 +9,9 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World\n"))
 	})
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok\n"))
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
